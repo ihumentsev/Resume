44d79a5c0179b1ce1af3746db263561f91a4fd99
ihumentsev <106383787+ihumentsev@users.noreply.github.com>
2023-04-22 17:34:20 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,8 @@
-import useLocalStorage from 'hooks/use-localstorage';
-import i18next from '../i18n';
-import { useTranslation } from 'react-i18next';
 import AboutUser from './AboutUser/AboutUser';
 import Layout from './Layout/Layout';
 import Profile from './Profile/Profile';
 
 export const App = () => {
-  // const { t } = useTranslation;
-  // const [language, setLanguage] = useLocalStorage('language', 'en');
-
-  // const handleLanguageChange = () => {
-  //   if (language === 'en') {
-  //     i18next.changeLanguage('pl');
-  //     setLanguage('pl');
-  //   } else if (language === 'pl') {
-  //     i18next.changeLanguage('en');
-  //     setLanguage('en');
-  //   }
-  // };
   return (
     <Layout>
       <Profile />
